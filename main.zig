60c07d300cc246e37433f907a6ac4a7e85719a37
Soroosh Chabi <soroosh.chabi@gmail.com>
2025-05-24 23:30:11 +0330

    Add sync and fix unlock

--- main.zig
+++ main.zig
@@ -8,6 +8,7 @@ pub fn main() !void {
     const allocator = dbg_alloc.allocator();
     var client: std.http.Client = .{ .allocator = allocator };
     defer client.deinit();
+    try sync(&client);
     if (try locked(allocator, &client)) {
         var passwd: [1024]u8 = undefined;
         const read = try getPasswd(&passwd);
@@ -45,68 +46,35 @@ pub fn getPasswd(buffer: []u8) !usize {
     return read - 1;
 }
 
-const server_header_buffer_size = 1024;
+fn sync(client: *std.http.Client) !void {
+    const fetch_options: std.http.Client.FetchOptions = .{ .method = .POST, .location = .{ .url = "http://localhost:8087/sync" } };
+    if ((try client.fetch(fetch_options)).status != .ok) {
+        return error.SyncFailed;
+    }
+}
 
-pub fn locked(allocator: std.mem.Allocator, client: *std.http.Client) !bool {
-    const status_uri = try std.Uri.parse("http://localhost:8087/status");
-    var server_header_buffer: [server_header_buffer_size]u8 = undefined;
-    var request = try client.open(.GET, status_uri, .{ .server_header_buffer = &server_header_buffer });
-    defer request.deinit();
-    try request.send();
-    try request.wait();
-    if (request.response.status != .ok) {
-        return error.BadHttpStatus;
+fn locked(allocator: std.mem.Allocator, client: *std.http.Client) !bool {
+    var response = std.ArrayList(u8).init(allocator);
+    defer response.deinit();
+    const fetch_options: std.http.Client.FetchOptions = .{ .keep_alive = false, .method = .GET, .location = .{ .url = "http://localhost:8087/status" }, .response_storage = .{ .dynamic = &response } };
+    if ((try client.fetch(fetch_options)).status != .ok) {
+        return error.StatusFailed;
     }
-    const response_size = 1024;
-    var response: [response_size]u8 = undefined;
-    const read = try request.read(&response);
-    if (read >= response_size) {
-        return error.HttpResponseTooLarge;
-    }
-    var parsed_response = try std.json.parseFromSlice(std.json.Value, allocator, response[0..read], .{});
+    var parsed_response = try std.json.parseFromSlice(std.json.Value, allocator, response.items, .{});
     defer parsed_response.deinit();
     const status = parsed_response.value.object.get("data").?.object.get("template").?.object.get("status").?.string;
     return std.mem.eql(u8, status, "locked");
 }
 
-pub fn unlock(allocator: std.mem.Allocator, client: *std.http.Client, passwd: []const u8) !void {
+fn unlock(allocator: std.mem.Allocator, client: *std.http.Client, passwd: []const u8) !void {
     var object = std.json.ObjectMap.init(allocator);
     defer object.deinit();
     try object.put("password", .{ .string = passwd });
     var payload = std.ArrayList(u8).init(allocator);
     defer payload.deinit();
     try std.json.stringify(std.json.Value{ .object = object }, .{}, payload.writer());
-    const status = try unlockFetch(payload.items, client);
-    if (status != .ok) {
+    const fetch_options: std.http.Client.FetchOptions = .{ .method = .POST, .location = .{ .url = "http://localhost:8087/unlock" }, .payload = payload.items, .headers = .{ .content_type = .{ .override = "application/json" } } };
+    if ((try client.fetch(fetch_options)).status != .ok) {
         return error.UnlockFailed;
     }
 }
-
-fn unlockFetch(payload: []const u8, client: *std.http.Client) !std.http.Status {
-    var server_header_buffer: [server_header_buffer_size]u8 = undefined;
-    const headers: std.http.Client.Request.Headers = .{ .content_type = .{ .override = "application/json" } };
-    const fetch_options: std.http.Client.FetchOptions = .{ .server_header_buffer = &server_header_buffer, .method = .POST, .location = .{ .url = "http://localhost:8087/unlock" }, .payload = payload, .headers = headers };
-    return (try client.fetch(fetch_options)).status;
-}
-
-// fn unlockRequest(payload: []const u8, client: *std.http.Client) !std.http.Status {
-//     const unlock_uri = try std.Uri.parse("http://localhost:8087/unlock");
-//     var server_header_buffer: [server_header_buffer_size]u8 = undefined;
-//     var request = try client.open(.POST, unlock_uri, .{ .server_header_buffer = &server_header_buffer });
-//     defer request.deinit();
-//     try request.send();
-//     try request.wait();
-//     if (request.response.status != .ok) {
-//         return error.BadHttpStatus;
-//     }
-//     const response_size = 1024;
-//     var response: [response_size]u8 = undefined;
-//     const read = try request.read(&response);
-//     if (read >= response_size) {
-//         return error.HttpResponseTooLarge;
-//     }
-//     var parsed_response = try std.json.parseFromSlice(std.json.Value, allocator, response[0..read], .{});
-//     defer parsed_response.deinit();
-//     const status = parsed_response.value.object.get("data").?.object.get("template").?.object.get("status").?.string;
-//     return std.mem.eql(u8, status, "locked");
-// }
